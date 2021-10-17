0a83be70a80f7dcb7063a13f825fc63fa3a96cf8
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-17 19:46:34 +0900

    fix MySQL用の書き方へ修正

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,25 +1,45 @@
 import {Entity, model, property} from '@loopback/repository';
 
-@model()
+@model({settings: {idInjection: false, mysql: {schema: 'demo', table: 'user'}}})
 export class User extends Entity {
   @property({
     type: 'number',
-    id: true,
-    generated: true,
+    required: true,
+    precision: 10,
+    scale: 0,
+    id: 1,
+    mysql: {columnName: 'id', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'N'},
   })
   id: number;
 
   @property({
     type: 'string',
+    length: 512,
+    mysql: {columnName: 'name', dataType: 'varchar', dataLength: 512, dataPrecision: null, dataScale: null, nullable: 'Y'},
   })
   name?: string;
 
   @property({
-    type: 'boolean',
+    type: 'number',
+    precision: 3,
+    scale: 0,
+    mysql: {columnName: 'hasAccount', dataType: 'tinyint', dataLength: null, dataPrecision: 3, dataScale: 0, nullable: 'Y'},
   })
-  hasAccount: boolean;
+  hasAccount?: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
 
   constructor(data?: Partial<User>) {
     super(data);
   }
 }
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
