6ae6e00bb5e9e74d120eccfc67f700d2e6cad9db
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-19 22:32:05 +0900

    fix 環境変数をdotenvで管理するよう修正

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -1,15 +1,17 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config = {
   name: 'db',
   connector: 'mysql',
   url: '',
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'demo'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 };
 
 // Observe application's life cycle to disconnect the datasource when
